24ab5a417d56a48101fcc1cdb71be20426bd2c11
Rabeles <9david@seznam.cz>
2022-07-06 00:22:28 +0200

    AssemblyScript contract almost done

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,8 +1,55 @@
-import { PersistentUnorderedMap,context } from "near-sdk-as";
- 
-export const voters = new PersistentUnorderedMap<string, bool>("VOTERS");
-export const Parties = new PersistentUnorderedMap<string, string>("PARTIES")
+import { PersistentUnorderedMap,context,PersistentVector} from "near-sdk-as";
+import { Party } from './model';
 
-export function CreateParty(id: string, productName: string): void {
-    Parties.set(id, productName);
+const voters = new PersistentUnorderedMap<string,string>("VOTERS");
+const Parties = new PersistentUnorderedMap<string, Party>("PARTIES");
+const owner = context.sender;
+
+export function CreateParty(party: Party): void {
+    let existingParty = Parties.get(party.id);
+    if (existingParty !== null) {
+        throw new Error(`A party with ${party.id} already exists`);
+    }
+    Parties.set(party.id, Party.fromPayload(party));
+}
+
+export function GetAllParties(): Party[] | null{
+    return Parties.values();
+}
+
+
+export function SetContextSender(): void{
+    voters.set(context.sender,"ANO");
+}
+
+export function GetAllVoters(): string | null{
+    return voters.get(context.sender);
+}
+
+export function VotePositive(party: Party): void{
+    let voterexist = voters.get(context.sender)
+    if(voterexist !== null){
+        throw new Error("You already voted in this elections");
+    }
+    let PartyToVote = Parties.get(party.id);
+    if (PartyToVote == null) {
+        throw new Error(`A party does not exists ${party.id}`);
+    }
+    PartyToVote.votePositive();
+    Parties.set(PartyToVote.id, PartyToVote);
+    voters.set(context.sender,"Voted");
+}
+
+export function voteNegative(party: Party): void{
+    let voterexist = voters.get(context.sender)
+    if(voterexist !== null){
+        throw new Error("You already voted in this elections");
+    }
+    let PartyToVote = Parties.get(party.id);
+    if (PartyToVote == null) {
+        throw new Error(`A party does not exists`);
+    }
+    PartyToVote.voteNegative();
+    Parties.set(PartyToVote.id, PartyToVote);
+    voters.set(context.sender,"Voted");
 }
--- assembly/model.ts
+++ assembly/model.ts
@@ -1,4 +1,4 @@
-import { PersistentUnorderedMap, u128, context } from "near-sdk-as";
+import {context } from "near-sdk-as";
 
 @nearBindgen
 export class Party {
@@ -9,6 +9,17 @@ export class Party {
     owner: string;
     positiveVote: u32;
     negativeVote: u32;
+    public static fromPayload(payload: Party): Party {
+        const party = new Party();
+        party.id = payload.id;
+        party.name = payload.name;
+        party.description = payload.description;
+        party.image = payload.image;
+        party.owner = context.sender;
+        party.positiveVote = 0;
+        party.negativeVote = 0;
+        return party;
+    }
     public votePositive(): void {
         this.positiveVote = this.positiveVote + 1;
     }
@@ -16,5 +27,3 @@ export class Party {
         this.negativeVote = this.negativeVote + 1;
     }
 }
-
-export const listedProducts = new PersistentUnorderedMap<string, Party>("LISTED_PRODUCTS");
